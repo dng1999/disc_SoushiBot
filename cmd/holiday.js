fbba604ccf8de9ccb936de5826f54752abf593f2
Dorothy Ng <dn1131@nyu.edu>
2019-12-26 11:24:02 -0500

    small christmas update

---
+++ cmd/holiday.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, args) => {
+  console.log("\x1b[32m%s\x1b[0m","!holiday - " + message.author.username + " - " + message.createdAt);
+
+  var quotes = ["Happy holidays!",
+                "||-delighted noises-||",
+                "||baba! -giggles-||",
+                "You will soon learn that holidays will only remind you of those who are gone.",
+                "I hope you have a warm place to return to for the holidays."
+              ];
+  var roll = Math.floor(Math.random() * quotes.length);
+  message.channel.send(quotes[roll]);
+};
+
+exports.help = {
+  name: "holiday",
+  description: "Wishes you well."
+};
