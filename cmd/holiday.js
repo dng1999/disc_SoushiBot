c5b8d67a2bfc965b285c0e8d540b639b3479d68b
Dorothy Ng <dn1131@nyu.edu>
2019-12-27 18:51:44 -0500

    spoiler warnings added to holiday

--- cmd/holiday.js
+++ cmd/holiday.js
@@ -2,8 +2,8 @@ exports.run = (client, message, args) => {
   console.log("\x1b[32m%s\x1b[0m","!holiday - " + message.author.username + " - " + message.createdAt);
 
   var quotes = ["Happy holidays!",
-                "||-delighted noises-||",
-                "||baba! -giggles-||",
+                "||-delighted noises-|| (EXODUS spoilers)",
+                "||baba! -giggles-|| (EXODUS spoilers)",
                 "You will soon learn that holidays will only remind you of those who are gone.",
                 "I hope you have a warm place to return to for the holidays."
               ];
